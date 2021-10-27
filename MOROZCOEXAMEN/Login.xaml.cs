644c1e8bf48163efc62484448ea6fe31ebc83919
María José Orozco Aucancela <majos378@hotmail.com>
2021-10-26 21:41:17 -0500

    Agregar archivos de proyecto.

---
+++ MOROZCOEXAMEN/Login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MOROZCOEXAMEN
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            string user = txtUsuario.Text;
+            string pass = txtPassword.Text;
+
+            if (user == "estudiante2021" && pass == "uisrael2021")
+            {
+                await Navigation.PushAsync(new Registro());
+            }
+            else
+            {
+                await DisplayAlert("Error", "Datos incorrectos", "OK");
+            }
+
+
+        }
+    }
+}
---
+++ MOROZCOEXAMEN/Registro.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MOROZCOEXAMEN
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string usuario, string Nombre, string Total)
+        {
+            InitializeComponent();
+            lblTexto.Text = "Usuario en linea: " + usuario;
+            txtNombre.Text = Nombre;
+            txtPagoMensual.Text = Total;
+        }
+
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double pagoinicial = Convert.ToDouble(txtMontoinicial.Text);
+                double montomensual = Convert.ToDouble(txtPagoMensual.Text);
+
+                double cuota = ((montomensual + 90) * 5) + pagoinicial;
+                txtPagoMensual.Text = cuota.ToString();
+                await DisplayAlert("Alerta", "Elemento Guardado con Exito", "Aceptar");
+                await Navigation.PushAsync(new Resumen(txtNombre.Text, txtPagoMensual.Text));
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Advertencia", "Ingrese informacion", "Aceptar");
+            }
+
+        }
+
+        }
+}
